d89cdea789ca0aba63e5fc7e9e33dcbbca83328b
Morish Kumar <morish.dtg@gmail.com>
2023-11-04 23:40:02 +0530

    checking

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Morish Kumar</h1>
+        <h1>Kumar</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
         <HeaderSocials />
